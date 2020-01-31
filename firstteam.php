0df75dfc5ff102fd5f746165790d37a941b67908
Siyam Muhammad <siyam.muhammad@student.manchester.ac.uk>
2020-01-31 10:34:27 +0000

    login

--- firstteam.php
+++ firstteam.php
@@ -1,10 +1,53 @@
 <?php
-// Use j69327bw as the public profile to connect to
+
 $servername = "dbhost.cs.man.ac.uk";
 $username = "j69327bw";
 $password = "Year1Project";
 
+// Create connection
 $conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-$conn->close();
+
+
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>Register</h2>
+  </div>
+
+  <form method="post" action="data.php">
+  	<div class="input-group">
+  	  <label>Username</label>
+  	  <input type="text" name="username" value="<?php echo $username; ?>">
+  	</div>
+  	<div class="input-group">
+  	  <label>Email</label>
+  	  <input type="email" name="email" value="<?php echo $email; ?>">
+  	</div>
+  	<div class="input-group">
+  	  <label>Password</label>
+  	  <input type="password" name="password_1">
+  	</div>
+  	<div class="input-group">
+  	  <label>Confirm password</label>
+  	  <input type="password" name="password_2">
+  	</div>
+  	<div class="input-group">
+  	  <button type="submit" class="btn" name="reg_user">Register</button>
+  	</div>
+  	<p>
+  		Already a member? <a href="login.php">Sign in</a>
+  	</p>
+  </form>
+</body>
+</html>
