796beebea7743bbb694b91f20d3dada5fb7f3cb5
a64696ss <sheikh.sabri@student.manchester.ac.uk>
2020-01-31 11:07:03 +0000

    shit

--- home.php
+++ home.php
@@ -1,73 +1,38 @@
 <?php
-   include("config.php");
-   session_start();
-
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form
-
-      $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']);
-
-      $sql = "SELECT user_id FROM users WHERE email = '$myusername' and password = '$mypassword'";
-      $result = mysqli_query($db,$sql);
-      $active = $row['active'];
-
-
-      // If result matched $myusername and $mypassword, table row must be 1 row
-
-      if($count == 1) {
-         session_register("myusername");
-         $_SESSION['login_user'] = $myusername;
-
-         header("location: welcome.php");
-      }else {
-         $error = "Your Login Name or Password is invalid";
-      }
-   }
+$username = $_POST['username'];
+$password = $_POST['password'];
+if (!empty($username) || !empty($password)) {
+ $host = "dbhost.cs.man.ac.uk";
+    $dbUsername = "j69327bw";
+    $dbPassword = "Year1Project";
+    //create connection
+    $conn = new mysqli($host, $dbUsername, $dbPassword);
+    if (mysqli_connect_error()) {
+     die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+    } else {
+     $SELECT = "SELECT email From users Where email = ? Limit 1";
+     $INSERT = "INSERT Into users (email, password) values($username,$password)";
+     //Prepare statement
+     $stmt = $conn->prepare($SELECT);
+     $stmt->bind_param("s", $email);
+     $stmt->execute();
+     $stmt->bind_result($email);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
+     if ($rnum==0) {
+      $stmt->close();
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("ssssii", $username, $password);
+      $stmt->execute();
+      echo "New record inserted sucessfully";
+     } else {
+      echo "Someone already register using this email";
+     }
+     $stmt->close();
+     $conn->close();
+    }
+} else {
+ echo "All field are required";
+ die();
+}
 ?>
-<html>
-
-   <head>
-      <title>Login Page</title>
-
-      <style type = "text/css">
-         body {
-            font-family:Arial, Helvetica, sans-serif;
-            font-size:14px;
-         }
-         label {
-            font-weight:bold;
-            width:100px;
-            font-size:14px;
-         }
-         .box {
-            border:#666666 solid 1px;
-         }
-      </style>
-
-   </head>
-
-   <body bgcolor = "#FFFFFF">
-
-      <div align = "center">
-         <div style = "width:300px; border: solid 1px #333333; " align = "left">
-            <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
-
-            <div style = "margin:30px">
-
-               <form action = "" method = "post">
-                  <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
-                  <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
-                  <input type = "submit" value = " Submit "/><br />
-               </form>
-
-               <div style = "font-size:11px; color:#cc0000; margin-top:10px"></div>
-
-            </div>
-
-         </div>
-
-      </div>
-
-   </body>
-</html>
