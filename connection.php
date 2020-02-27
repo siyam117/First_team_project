956ad4894643353f55947fd0c082b279b8341222
Bence Webber <bence.webber@student.manchester.ac.uk>
2020-02-27 00:19:28 +0000

    Changed original title

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
 
-  $conn = new PDO("mysql:host=localhost;dbname=first_team_project", "root", "");
+  $conn = new PDO("mysql:host=localhost;dbname=first_team_project", "root", "Distinguished123!");
 
 ?>
