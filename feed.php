35da26c986b003894790eb8d2e8c73624c04c834
Bence Webber <bence.webber@student.manchester.ac.uk>
2020-02-28 13:54:05 +0000

    Small UI changes

--- feed.php
+++ feed.php
@@ -22,16 +22,13 @@
   <head>
 	<link rel="stylesheet" href="assets/css/styles.css">
   <link rel="stylesheet" href="assets/css/feed.css">
-  <link rel="stylesheet" href="assets/css/title_animation2.css">
 	<meta charset="UTF-8">
-    <title></title>
+    <title>Feed</title>
 	</head>
     <body>
-    	<div class="title" id="title-main">INKKER.IO</div>
-      <div class="title" id="title-shadow-one">INKKER.IO</div>
-      <div class="title" id="title-shadow-two">INKKER.IO</div>
-      <h1>You're in feed</h1><br>
-      <button class="submit-button" onclick="window.location.href = 'logout.php';">Log out</button><br>
+      <div id="header">
+        <button class="header-button" onclick="window.location.href = 'logout.php';">Log out</button><br>
+      </div>
       <div id="feed-box">
         <a class="feed-button" id="feed-button-addstory" href="settings.php"><div class="feed-button-text">Create new story</div></a>
         <?php
