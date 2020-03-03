3113cfc57877a85c2ec54cd043e797dc6c4a6b2b
a64696ss <sheikh.sabri@student.manchester.ac.uk>
2020-03-03 13:04:08 +0000

    kkk

--- join.php
+++ join.php
@@ -6,7 +6,7 @@
   <body>
     <form id="homepage-loginbox" action="private_lobby.php" method="post">
     <input class="input-field" id="username-field" type="text" autocomplete="off" placeholder="Enter PIN" name="lobby_id">
-    <button class="submit-button" id="login-button" type="submit" name="button" disabled>Enter <i class="fas fa-sign-in-alt"></i></button>
+    <button class="submit-button" id="login-button" type="submit" name="button" >Enter <i class="fas fa-sign-in-alt"></i></button>
   </form>
     <button type="button" name="button" onclick="window.location.href='private_lobby.php'">Create a new game</button>
   </body>
