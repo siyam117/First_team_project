7e4be67f474b13f103eac40c49dc2bdf30b45abb
Bence Webber <bence.webber@student.manchester.ac.uk>
2020-03-03 13:05:17 +0000

    Small changes

--- join.php
+++ join.php
@@ -4,9 +4,9 @@
     <title>Join Game</title>
   </head>
   <body>
-    <form id="homepage-loginbox" action="private_lobby.php" method="post">
-    <input class="input-field" id="username-field" type="text" autocomplete="off" placeholder="Enter PIN" name="lobby_id">
-    <button class="submit-button" id="login-button" type="submit" name="button" disabled>Enter <i class="fas fa-sign-in-alt"></i></button>
+    <form action="private_lobby.php" method="post">
+    <input type="text" autocomplete="off" placeholder="Enter PIN" name="lobby_id">
+    <button type="submit" name="button">Enter</button>
   </form>
     <button type="button" name="button" onclick="window.location.href='private_lobby.php'">Create a new game</button>
   </body>
