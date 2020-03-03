8ffde0b30dff438f287cb3a390cc0c25eca195ef
Bence Webber <bence.webber@student.manchester.ac.uk>
2020-03-03 18:06:27 +0000

    Fixed private_editor name

--- private_lobby.php
+++ private_lobby.php
@@ -39,6 +39,6 @@
     <title>Lobby</title>
   </head>
   <body>
-    <button type="button" name="button" onclick="window.location.href='private.php'">Start Game</button>
+    <button type="button" name="button" onclick="window.location.href='private_editor.php'">Start Game</button>
   </body>
 </html>
