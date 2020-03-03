78148d02a37b32ea91c13fe9e9354dc865aed148
Bence Webber <bence.webber@student.manchester.ac.uk>
2020-03-03 13:08:33 +0000

    Fixes

--- join.php
+++ join.php
@@ -4,16 +4,10 @@
     <title>Join Game</title>
   </head>
   <body>
-<<<<<<< HEAD
     <form action="private_lobby.php" method="post">
-    <input type="text" autocomplete="off" placeholder="Enter PIN" name="lobby_id">
-    <button type="submit" name="button">Enter</button>
-=======
-    <form id="homepage-loginbox" action="private_lobby.php" method="post">
-    <input class="input-field" id="username-field" type="text" autocomplete="off" placeholder="Enter PIN" name="lobby_id">
-    <button class="submit-button" id="login-button" type="submit" name="button" >Enter <i class="fas fa-sign-in-alt"></i></button>
->>>>>>> 3113cfc57877a85c2ec54cd043e797dc6c4a6b2b
-  </form>
+      <input type="text" autocomplete="off" placeholder="Enter PIN" name="lobby_id">
+      <button type="submit" name="button" >Enter</button>
+    </form>
     <button type="button" name="button" onclick="window.location.href='private_lobby.php'">Create a new game</button>
   </body>
 </html>
