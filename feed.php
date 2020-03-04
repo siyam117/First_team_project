4eebcd684a227c4bbf685f35ee434ef90f93f72c
Siyam Muhammad <siyam.muhammad@student.manchester.ac.uk>
2020-03-04 21:07:15 +0000

    search bar unfinished

--- feed.php
+++ feed.php
@@ -13,6 +13,37 @@
     header("Location: index.php");
   }
 
+if (isset($_POST["Go!"])){
+    $query = $_POST["search"];
+    $sth = $conn->prepare("SELECT title FROM stories WHERE title = '$query'");
+
+    $sth->setFetchMode(PDO:: FETCH_OBJ);
+    $sth->execute();
+
+    if($row = $sth->fetch())
+    {
+        ?>
+        <br><br><br>
+        <table>
+            <tr>
+                <th>title found!</th>
+            </tr>
+            <tr>
+                <td><?php echo $row->title; ?></td>
+            </tr>
+
+        </table>
+<?php
+
+    }
+        else{
+            echo "Name does not exist";
+
+        }
+
+}
+
+
 ?>
 
 
@@ -20,12 +51,40 @@
 <!DOCTYPE html>
 <html>
   <head>
+  <link rel="stylesheet" href="assets/css/style_search.css">
 	<link rel="stylesheet" href="assets/css/styles.css">
   <link rel="stylesheet" href="assets/css/feed.css">
   <link rel="stylesheet" href="assets/css/hamburger.css">
   <link id="animation-stylesheet" rel="stylesheet" href="assets/css/title_animation.css">
   <script type="text/javascript" src="assets/js/lib/jquery-3.4.1.min.js"></script>
 	<meta charset="UTF-8">
+  
+    <title>Search Bar using PHP</title>
+        <script type = "text/javascript">
+            function active(){
+                var SearchBar = document.getElementById('SearchBar');
+
+                if(SearchBar.value == 'Search...'){
+                    SearchBar.value = ''
+                    SearchBar.placeholder = 'Search...'
+                }
+            }
+            function inactive(){
+                var SearchBar = document.getElementById('SearchBar');
+
+                if(SearchBar.value == ''){
+                    SearchBar.value = 'Search...'
+                    SearchBar.placeholder = ''
+                }
+            }
+        
+        </script> 
+
+    <form method="post">
+    <input type ="text" name="search" id="SearchBar" placeholder="Search" value="Search..." maxlength="35" autocomplete="off" onmousedown="active();" onblur="inactive();" /><input type="submit" id="SearchBtn" name="Go!"/>
+
+    </form>
+
     <title>Feed</title>
 	</head>
     <body>
