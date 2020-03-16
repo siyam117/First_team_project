9df65f32b29bad76d6b13c205fac1d1bcc96f448
Bence Webber <bence.webber@student.manchester.ac.uk>
2020-03-16 17:51:07 +0000

    Added css and header to private gamemode

--- private_lobby.php
+++ private_lobby.php
@@ -38,11 +38,75 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Lobby</title>
-
+    <link rel="stylesheet" href="assets/css/master.css">
     <script type="text/javascript" src="assets/js/lib/jquery-3.4.1.min.js"></script>
+
+    <title>Lobby</title>
   </head>
   <body>
+
+
+    <!-- HEADER START -->
+    <div id="header">
+
+      <!-- DROPDOWN START -->
+      <?php
+
+      if (func::checkLoginState($conn))
+      {
+
+        echo '<div class="dropdown">';
+
+          echo '<div class="hamburger-container">';
+            echo '<div class="hamburger">';
+              echo '<span class="bar"></span>';
+              echo '<span class="bar"></span>';
+              echo '<span class="bar"></span>';
+            echo '</div>';
+          echo '</div>';
+
+          echo '<div class="body">';
+            echo '<div class="inner-body">';
+
+              echo '<div class="section">';
+
+                $user_id = $_COOKIE["user_id"];
+                echo "<a class='dropdown-button' href='profile.php?id=$user_id'>MY PROFILE <i class='fas fa-user'></i></a>";
+
+              echo '</div>';
+
+              echo '<div class="section">';
+                echo '<a class="dropdown-button" href="logout.php">LOG OUT</a>';
+              echo '</div>';
+
+            echo '</div>';
+          echo '</div>';
+
+        echo '</div>';
+
+      }
+
+      ?>
+      <!-- DROPDOWN END -->
+
+      <!-- TITLE START -->
+      <a id="header-title" href="index.php">
+        <div class="glitch-container">
+          <div class="glitch-text" id="glitch-main">INKKER.IO</div>
+          <div class="glitch-text" id="glitch-shadow-one">INKKER.IO</div>
+          <div class="glitch-text" id="glitch-shadow-two">INKKER.IO</div>
+        </div>
+      </a>
+      <!-- TITLE END -->
+
+    </div>
+    <!-- HEADER END -->
+
+
+
+
+
+
     <?php
 
     if ($creator_user_id == $private_user_id)
--- private_options.php
+++ private_options.php
@@ -3,32 +3,24 @@
   include_once("../connection.php");
   include_once("functions.php");
 
-  if (func::checkLoginState($conn))
+
+  if (isset($_POST["create"]) && isset($_POST["username"]))
   {
+    $username = $_POST["username"];
+    $lobby_id = func::generateUniqueLobbyID($conn);
 
-    if (isset($_POST["create"]) && isset($_POST["username"]))
+    if ($lobby_id != null)
     {
-      $username = $_POST["username"];
-      $lobby_id = func::generateUniqueLobbyID($conn);
+      $private_user_id = func::addNewUserPRIVATE($conn, $username, $lobby_id);
 
-      if ($lobby_id != null)
+      if ($private_user_id != null)
       {
-        $private_user_id = func::addNewUserPRIVATE($conn, $username, $lobby_id);
+        func::startNewSessionPRIVATE($conn, $private_user_id);
+        $conn->exec("INSERT INTO private_stories (lobby_id, creator_user_id, title) VALUES ('$lobby_id', '$private_user_id', 'Untitled');");
 
-        if ($private_user_id != null)
-        {
-          func::startNewSessionPRIVATE($conn, $private_user_id);
-          $conn->exec("INSERT INTO private_stories (lobby_id, creator_user_id, title) VALUES ('$lobby_id', '$private_user_id', 'Untitled');");
-
-          header("Location: private_lobby.php?id=$lobby_id");
-        }
+        header("Location: private_lobby.php?id=$lobby_id");
       }
     }
-
-  }
-  else
-  {
-    header("Location: index.php");
   }
 
 ?>
@@ -38,9 +30,73 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" href="assets/css/master.css">
+    <script type="text/javascript" src="assets/js/lib/jquery-3.4.1.min.js"></script>
+
     <title>Private Games</title>
   </head>
   <body>
+
+
+    <!-- HEADER START -->
+    <div id="header">
+
+      <!-- DROPDOWN START -->
+      <?php
+
+      if (func::checkLoginState($conn))
+      {
+
+        echo '<div class="dropdown">';
+
+          echo '<div class="hamburger-container">';
+            echo '<div class="hamburger">';
+              echo '<span class="bar"></span>';
+              echo '<span class="bar"></span>';
+              echo '<span class="bar"></span>';
+            echo '</div>';
+          echo '</div>';
+
+          echo '<div class="body">';
+            echo '<div class="inner-body">';
+
+              echo '<div class="section">';
+
+                $user_id = $_COOKIE["user_id"];
+                echo "<a class='dropdown-button' href='profile.php?id=$user_id'>MY PROFILE <i class='fas fa-user'></i></a>";
+
+              echo '</div>';
+
+              echo '<div class="section">';
+                echo '<a class="dropdown-button" href="logout.php">LOG OUT</a>';
+              echo '</div>';
+
+            echo '</div>';
+          echo '</div>';
+
+        echo '</div>';
+
+      }
+
+      ?>
+      <!-- DROPDOWN END -->
+
+      <!-- TITLE START -->
+      <a id="header-title" href="index.php">
+        <div class="glitch-container">
+          <div class="glitch-text" id="glitch-main">INKKER.IO</div>
+          <div class="glitch-text" id="glitch-shadow-one">INKKER.IO</div>
+          <div class="glitch-text" id="glitch-shadow-two">INKKER.IO</div>
+        </div>
+      </a>
+      <!-- TITLE END -->
+
+    </div>
+    <!-- HEADER END -->
+
+
+
+
     <form action="private_username.php" method="get">
       <input type="text" autocomplete="off" placeholder="Enter PIN" name="id">
       <button type="submit">Enter</button>
