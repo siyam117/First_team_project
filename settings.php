66a434f1a19a89462849269345cbc70c34ef025b
Sheikh Ameer Sabri <sheikh.sabri@student.manchester.ac.uk>
2020-03-20 13:42:24 -0700

    it should be working now high hopes

--- settings.php
+++ settings.php
@@ -121,14 +121,14 @@
 
         <h>Create a new story</h><br> -->
 			
-			<div>
+			<div class="slider-hold">
 			<form action="settings.php" method="POST" id="settingspage-box">
 				<input class="input-field" id="username-field" type="text" autocomplete="off" placeholder="Title" name="title">
 				
 				<label class ="settings_text">Number Of Sections</label>
 				<span id="rangeValue">0</span>
-				<script type="text/javascript" src="assets/js/main.js"></script>
-				<input class="range" id="username-field" type="range" autocomplete="off" min="5" max = "20" value="5" placeholder="Number of Sections " name="section_amount">
+				<script type="text/javascript" src="assets/js/slider.js"></script>
+				<input class="range" id="username-field" type="range" min="5" max = "20" value="5" name="section_amount">
 				
 				<label class = "settings_text">Section Length</label>
 				<input class="range" id="username-field" type="range" autocomplete="off" min="10" step="5" max = "300" value="10" placeholder="Section Length (1000 Max)" name="section_length">
